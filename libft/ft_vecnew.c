44861cf5fef6e99362490292c2235a9d58eeb810
Aleksandr Tikka <altikka@student.hive.fi>
2022-04-25 18:56:39 +0300

    Create a 'fresh' vector with wanted initial size.

---
+++ libft/ft_vecnew.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vecnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 09:33:48 by altikka           #+#    #+#             */
+/*   Updated: 2022/04/25 15:11:38 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec.h"
+
+int	ft_vecnew(t_vec *dest, size_t init_len, size_t type)
+{
+	if (!dest || type == 0)
+		return (-1);
+	dest->alloc_size = type * init_len;
+	dest->type = type;
+	dest->len = 0;
+	if (init_len == 0)
+		dest->data = NULL;
+	else
+	{
+		dest->data = malloc(dest->alloc_size);
+		if (!dest->data)
+			return (-1);
+	}
+	return (0);
+}
