dd6199e75ab3460406db15f11369813335884153
Aleksandr Tikka <altikka@student.hive.fi>
2022-04-25 19:03:26 +0300

    Add one element to the end of the vector. Resizes if needed.

---
+++ libft/ft_vecpush.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vecpush.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 17:26:52 by altikka           #+#    #+#             */
+/*   Updated: 2022/04/25 18:03:55 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec.h"
+
+int	ft_vecpush(t_vec *dest, void *src)
+{
+	if (!dest || !src)
+		return (-1);
+	if (!dest->data)
+		ft_vecnew(dest, 1, dest->type);
+	if (dest->type * dest->len >= dest->alloc_size)
+		if (ft_vecresize(dest, dest->alloc_size * 2) < 0)
+			return (-1);
+	ft_memcpy(&dest->data[dest->type * dest->len], src, dest->type);
+	dest->len++;
+	return (0);
+}
