40d8b6b43c9f55822d4e2bdf355898fc531fcf99
Aleksandr Tikka <altikka@student.hive.fi>
2022-04-27 20:00:44 +0300

    trying to first get a character string from print_shop and then write it at once

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,19 +6,36 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 14:38:37 by altikka           #+#    #+#             */
-/*   Updated: 2022/04/22 17:58:38 by altikka          ###   ########.fr       */
+/*   Updated: 2022/04/27 18:22:54 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+													[|]
+#	%[ $ ][ flags ][width][.precision][  lenght  ]conversion
+#	       '#-+ 0'	all	   !c & !p	  			  c s p
+#									   hh,h,l,ll  d i o u x X
+#									   l,L	      f
+
+*/
 
 #include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-	t_stats	attr;
+	char	*p;
 	int		len;
 
-	attr = NULL;
-	len = 0;
+	va_start(ap, format);
+	len = print_shop(&p, format, ap);
+	va_end(ap);
+	if (len < 0)
+	{
+		ft_putendl("print shop burned down");
+		return (len);
+	}
+	write(1, p, len);
+	free(p);
 	return (len);
 }
