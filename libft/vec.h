e3d1633f9825541ea2a2be5e641acead0e1389f9
Aleksandr Tikka <altikka@student.hive.fi>
2022-04-27 14:42:46 +0300

    cleaner

--- libft/vec.h
+++ libft/vec.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 09:19:35 by altikka           #+#    #+#             */
-/*   Updated: 2022/04/25 18:54:15 by altikka          ###   ########.fr       */
+/*   Updated: 2022/04/27 14:36:00 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ typedef struct s_vec
 	size_t	alloc_size;	/* total size of allocated bytes */
 	size_t	type;		/* size of the vec elemement (type) in bytes */
 	size_t	len;		/* len of used-up mem in type chunck(s) */
-}		t_vec;
+}			t_vec;
 
 int		ft_vecnew(t_vec *dest, size_t init_len, size_t type);
 void	ft_vecdel(t_vec *src);
 int		ft_veccpy(t_vec *dest, t_vec *src);
 int		ft_vecresize(t_vec *src, size_t new_size);
 int		ft_vecpush(t_vec *dest, const void *src);
-int		ft_vecappend(t_vec *dest, t_vec *src); //...
-int		ft_vecncat(t_vec *dest, const void *src, size_t n); //...
+int		ft_vecappend(t_vec *dest, t_vec *src);
+int		ft_vecncat(t_vec *dest, const void *src, size_t n);
 
 #endif
