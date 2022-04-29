a0ba5b95a3202c754e216dbb1dbe32989d027a02
Aleksandr Tikka <altikka@student.hive.fi>
2022-04-29 15:54:15 +0300

    initializing stats struct before going to press.c

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 14:08:42 by altikka           #+#    #+#             */
-/*   Updated: 2022/04/29 13:18:59 by altikka          ###   ########.fr       */
+/*   Updated: 2022/04/29 15:40:35 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ typedef struct s_stat
 {
 	va_list			ap;
 	long			value;
-	char			type;
 	int				sign;
 	unsigned int	is_signed;	// : 1?
 	unsigned int	flags;		// : 5?
 	unsigned int	width;
 	unsigned int	preci;
 	unsigned int	preci_on;	// : 1?
-	unsigned int	lenght;		// : ??
+	unsigned int	lenght;		// : 3?
+	char			type;
 	char			pad;
 }			t_stat;
 
@@ -48,6 +48,8 @@ t_STRUCT (*)NAME[4] = {
 
 int		ft_printf(const char *format, ...);
 int		print_shop(char **str, const char *format, va_list ap);
+void	init_stats(t_stat *attr, va_list ap);
+void	reset_stats(t_stat *attr);
 int		press(t_vec *dest, const char *format, t_stat attr);
 
 #endif
--- src/print_shop.c
+++ src/print_shop.c
@@ -6,32 +6,11 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 18:36:48 by altikka           #+#    #+#             */
-/*   Updated: 2022/04/28 17:07:14 by altikka          ###   ########.fr       */
+/*   Updated: 2022/04/29 15:38:25 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-//SEPERATE IN OWN .C
-int	press(t_vec *dest, const char *format, t_stat attr)
-{
-	const char	*p;
-
-	attr.value = 666;	//init_stats(&attr);
-	p = format;
-	while (*p)
-	{
-		if (*p == '%')
-		{
-			ft_vecncat(dest, format, (p - format));
-			//FT_WHAT_NOW;
-			format = p;
-		}
-		p++;
-	}
-	ft_vecncat(dest, format, (p - format));
-	return (0);
-}
 
 int	print_shop(char **str, const char *format, va_list ap)
 {
@@ -42,7 +21,7 @@ int	print_shop(char **str, const char *format, va_list ap)
 	va_end(ap);			//REMOVE
 	if (ft_vecnew(&print, 1, sizeof(char)) < 0)
 		return (-1);
-	attr.value = 666;	//init_stats(&attr);
+	init_stats(&attr, ap);
 	ret = press(&print, format, attr);
 	if (ret < 0)
 		return (ret);
