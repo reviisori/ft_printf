a5b96045bdfdeadca086ff83dacaee00515b7130
Aleksandr Tikka <altikka@student.hive.fi>
2022-04-29 15:55:29 +0300

    press.c own file now, rdy-like while loop to start looking for %-conversions

---
+++ src/press.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   press.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 14:21:41 by altikka           #+#    #+#             */
+/*   Updated: 2022/04/29 15:40:02 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	press(t_vec *dest, const char *format, t_stat attr)
+{
+	const char	*p;
+
+	p = format;
+	while (*p)
+	{
+		if (*p == '%')
+		{
+			if (ft_vecncat(dest, format, (p - format)) < 0)
+				return (-1);
+			reset_stats(&attr);
+			//FT_WHAT_NOW;
+			format = p;
+		}
+		p++;
+	}
+	if (ft_vecncat(dest, format, (p - format)) < 0)
+		return (-1);
+	return (0);
+}
