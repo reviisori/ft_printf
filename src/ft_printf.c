2cdd5b036c6f1f9b2a76f19e6027b18ce51597c5
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-02 17:11:04 +0300

    more easily detectable error message

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 14:38:37 by altikka           #+#    #+#             */
-/*   Updated: 2022/04/27 18:22:54 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/02 16:28:43 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	ft_printf(const char *format, ...)
 	va_end(ap);
 	if (len < 0)
 	{
-		ft_putendl("print shop burned down");
+		ft_putendl("*ERROR*: print shop burned down");
 		return (len);
 	}
 	write(1, p, len);
