424c3cfa6bcf20ea62ede94a4b292ef0f2d94c37
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-02 17:17:13 +0300

    hack&slash version with correct indexing and minimal error/edge case handling

--- src/press_type.c
+++ src/press_type.c
@@ -1,26 +1,42 @@
-//42-HEADER
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   press_type.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 13:17:17 by altikka           #+#    #+#             */
+/*   Updated: 2022/05/02 16:26:54 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
 static int	get_index(const char c)
 {
-	char	type[];
+	char	*p;
 
-	type = "cdfiopsux";	//define in .h?
-	return (INDEX);		//YEP
+	p = ft_strcasechr(TYPE_FIELD, c);
+	if (!p)
+		return (-1);
+	return (p - TYPE_FIELD);
 }
 
 int	press_type(t_vec *dest, const char **fptr, t_stat *attr)
 {
 	t_jump	disp;
+	int		i;
 
-	*fptr++;
-	attr->type = *fptr;
-	if (disp[get_index(attr->type)] < 0)
+	(*fptr)++;
+	attr->type = **fptr;
+	(*fptr)++;
+	i = get_index(attr->type);
+	if (i < 0)
 	{
-		ft_putendl("error: conversion not supported");
+		ft_putendl("*ERROR*: conversion not supported");
 		return (-1);
 	}
+	disp = g_jump[i];
 	return (disp(dest, attr));
 }
