9292ee71aa27141c2e1980dca6fd3f9dc672be4f
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-05 12:30:29 +0300

    added get_base function

--- src/utilities.c
+++ src/utilities.c
@@ -6,11 +6,21 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 14:30:47 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/05 09:47:33 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/05 12:24:32 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	set_base(const char c)
+{
+	if (c == 'x' || c == 'X' || c == 'p')
+		return (16);
+	else if (c == 'o')
+		return (8);
+	else
+		return (10);
+}
 
 void	init_stats(t_stat *attr, va_list ap)
 {
@@ -22,11 +32,15 @@ void	reset_stats(t_stat *attr)
 {
 	attr->val = 0;
 	attr->sign = 0;
-	attr->is_signed = 0;
-	attr->flags = 0;
+	attr->is_signed = false;
+	attr->hash = false;
+	attr->dash = false;
+	attr->plus = false;
+	attr->space = false;
+	attr->zero = false;
 	attr->width = 0;
 	attr->preci = 0;
-	attr->preci_on = 0;
+	attr->preci_on = false;
 	attr->lenght = 0;
 	attr->type = ' ';
 	attr->pad = ' ';
