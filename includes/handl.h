5b8b8f3f053b90ae5195e2cdb5cffeed09ab6b14
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-05 16:26:03 +0300

    moved t_stat here. Greenlit all the handlers and their jump table

--- includes/handl.h
+++ includes/handl.h
@@ -6,13 +6,15 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 10:28:17 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/05 11:22:13 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/05 16:17:07 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HANDL_H
 
 # define HANDL_H
+
+# include <stdbool.h>
 
 typedef enum e_len
 {
@@ -24,8 +26,25 @@ typedef enum e_len
 	L
 }			t_len;
 
-/*
- *
+typedef struct s_stat
+{
+	va_list			ap;
+	long			val;
+	int				sign;
+	bool			is_signed;
+	bool			hash;
+	bool			dash;
+	bool			plus;
+	bool			space;
+	bool			zero;
+	unsigned int	width;
+	unsigned int	preci;
+	bool			preci_on;
+	t_len			lenght;
+	char			type;
+	char			pad;
+}			t_stat;
+
 char	*handle_char(t_stat *attr);
 char	*handle_short(t_stat *attr);
 char	*handle_int(t_stat *attr);
@@ -41,7 +60,5 @@ static const t_handler g_handler[6] = {
 	handle_long,
 	handle_long
 };
- *
- */
 
 #endif
