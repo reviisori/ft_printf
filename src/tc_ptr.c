e402dfa7f07e3c84b227efc0da129ff06df105a2
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-06 12:55:30 +0300

    prefix for ptr done quick and dirty

--- src/tc_ptr.c
+++ src/tc_ptr.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:12:42 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/02 16:12:56 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/06 12:51:19 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 int	tc_ptr(t_vec *dest, t_stat *attr)
 {
 	attr->lenght = l;
+	if (ft_vecncat(dest, "0x", 2) < 0)
+		return (-1);
 	return (tc_num(dest, attr));
 }
