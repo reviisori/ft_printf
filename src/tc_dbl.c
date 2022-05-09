3c6331005a71f22efd364b6d7e6a3937a968a9a7
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-09 14:30:51 +0300

    first phase of restructioning

--- src/tc_dbl.c
+++ src/tc_dbl.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:10:57 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/09 13:59:06 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/09 14:30:12 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 #include "libft.h"
 #include "vec.h"
 
-static int	pad_dbl(t_vec *dest, size_t dec_len, unsigned int preci)
+static int	pad_dbl(t_vec *dest, size_t dec_len, t_stat *info)
 {
 	unsigned int	pad;
 
-	pad = preci - dec_len;
+	pad = info->preci - dec_len;
 	while (pad-- > 0)
 		if (ft_vecpush(dest, "0") < 0)
 			return (-1);
 	return (1);
 }
 
-static int	add_parts(t_vec *dest, char *whl, char *dec, unsigned int preci)
+static int	add_parts(t_vec *dest, char *whl, char *dec, t_stat *info)
 {
 	size_t	whl_len;
 	size_t	dec_len;
@@ -41,7 +41,7 @@ static int	add_parts(t_vec *dest, char *whl, char *dec, unsigned int preci)
 	ft_strdel(&whl);
 	if (ft_vecpush(dest, ".") < 0)
 		return (-1);
-	if (pad_dbl(dest, dec_len, preci) < 0)
+	if (pad_dbl(dest, dec_len, info) < 0)
 		return (-1);
 	if (ft_vecncat(dest, dec, dec_len) < 0)
 	{
@@ -52,8 +52,7 @@ static int	add_parts(t_vec *dest, char *whl, char *dec, unsigned int preci)
 	return (1);
 }
 
-static int	get_and_round(t_vec *dest, long double val,
-					unsigned int preci, t_stat *info)
+static int	get_and_round(t_vec *dest, long double val, t_stat *info)
 {
 	char			*whl;
 	char			*dec;
@@ -61,12 +60,12 @@ static int	get_and_round(t_vec *dest, long double val,
 	unsigned int	mdl;
 
 	whl = ft_anytoa((unsigned long ) val, 10, info->sign, false);
-	d = (val - (unsigned long ) val) * ft_pow(10, preci + 1);
+	d = (val - (unsigned long ) val) * ft_pow(10, info->preci + 1);
 	mdl = (unsigned long ) d % 10;
 	if (mdl >= 5)
 	{
 		d = (d / 10) + 1;
-		if ((unsigned int ) ft_intlen(d) > preci)
+		if ((unsigned int ) ft_intlen(d) > info->preci)
 		{
 			ft_strdel(&whl);
 			whl = ft_anytoa((unsigned long ) val + 1, 10, info->sign, false);
@@ -76,23 +75,22 @@ static int	get_and_round(t_vec *dest, long double val,
 	else
 		d /= 10;
 	dec = ft_anytoa((unsigned long ) d, 10, 1, false);
-	return (add_parts(dest, whl, dec, preci));
+	return (add_parts(dest, whl, dec, info));
 }
 
 int	tc_dbl(t_vec *dest, t_stat *info)
 {
 	long double		val;
-	unsigned int	preci;
 	t_vec			temp;
 
 	val = va_arg(info->ap, double);
 	info->sign = 1 - 2 * (val < 0);
 	if (val < 0)
-		val *= -1;
-	preci = 6 * !info->preci_on + info->preci;
+		val *= info->sign;
+	info->preci = 6 * !info->preci_on + info->preci;
 	if (ft_vecnew(&temp, 1, sizeof(char)) < 0)
 		return (-1);
-	if (get_and_round(&temp, val, preci, info) < 0)
+	if (get_and_round(&temp, val, info) < 0)
 		return (-1);
 	if (ft_vecappend(dest, &temp) < 0)
 		return (-1);
