f062f74db05f79e516f38fec9a229dced20ec92c
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-09 15:03:40 +0300

    leave it alone for now

--- src/tc_dbl.c
+++ src/tc_dbl.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:10:57 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/09 14:30:12 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/09 15:03:09 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,30 +52,34 @@ static int	add_parts(t_vec *dest, char *whl, char *dec, t_stat *info)
 	return (1);
 }
 
-static int	get_and_round(t_vec *dest, long double val, t_stat *info)
+static int	get_parts(t_vec *dest, long double val, long double d, t_stat *info)
 {
-	char			*whl;
-	char			*dec;
+	char	*whl;
+	char	*dec;
+
+	if ((unsigned int ) ft_intlen(d) > info->preci)
+	{
+		whl = ft_anytoa((unsigned long ) val + 1, 10, info->sign, false);
+		d = 0;
+	}
+	else
+		whl = ft_anytoa((unsigned long ) val, 10, info->sign, false);
+	dec = ft_anytoa((unsigned long ) d, 10, 1, false);
+	return (add_parts(dest, whl, dec, info));
+}
+
+static int	round_dbl(t_vec *dest, long double val, t_stat *info)
+{
 	long double		d;
 	unsigned int	mdl;
 
-	whl = ft_anytoa((unsigned long ) val, 10, info->sign, false);
 	d = (val - (unsigned long ) val) * ft_pow(10, info->preci + 1);
 	mdl = (unsigned long ) d % 10;
 	if (mdl >= 5)
-	{
 		d = (d / 10) + 1;
-		if ((unsigned int ) ft_intlen(d) > info->preci)
-		{
-			ft_strdel(&whl);
-			whl = ft_anytoa((unsigned long ) val + 1, 10, info->sign, false);
-			d = 0;
-		}
-	}
 	else
 		d /= 10;
-	dec = ft_anytoa((unsigned long ) d, 10, 1, false);
-	return (add_parts(dest, whl, dec, info));
+	return (get_parts(dest, val, d, info));
 }
 
 int	tc_dbl(t_vec *dest, t_stat *info)
@@ -90,7 +94,7 @@ int	tc_dbl(t_vec *dest, t_stat *info)
 	info->preci = 6 * !info->preci_on + info->preci;
 	if (ft_vecnew(&temp, 1, sizeof(char)) < 0)
 		return (-1);
-	if (get_and_round(&temp, val, info) < 0)
+	if (round_dbl(&temp, val, info) < 0)
 		return (-1);
 	if (ft_vecappend(dest, &temp) < 0)
 		return (-1);
