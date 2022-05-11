396853854d81c1e555a5b96b7506394212498711
home <aleksandr.tikka@gmail.com>
2022-05-11 22:10:30 +0300

    clearer struction, less nonsense

--- src/inves_lenght.c
+++ src/inves_lenght.c
@@ -6,26 +6,11 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 14:09:25 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/11 21:56:08 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/11 22:09:14 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static int	hotel(const char **fptr, t_stat *info)
-{
-	if (*(*fptr + 1) == 'h')
-	{
-		info->lenght = hh;
-		(*fptr) = (*fptr) + 2;
-	}
-	else
-	{
-		info->lenght = h;
-		(*fptr)++;
-	}
-	return (1);
-}
 
 static int	lima(const char **fptr, t_stat *info)
 {
@@ -42,10 +27,23 @@ static int	lima(const char **fptr, t_stat *info)
 	return (1);
 }
 
+static int	hotel(const char **fptr, t_stat *info)
+{
+	if (*(*fptr + 1) == 'h')
+	{
+		info->lenght = hh;
+		(*fptr) = (*fptr) + 2;
+	}
+	else
+	{
+		info->lenght = h;
+		(*fptr)++;
+	}
+	return (1);
+}
+
 int	inves_lenght(const char **fptr, t_stat *info)
 {
-	if (!(**fptr == 'h' || **fptr == 'l' || **fptr == 'L'))
-		return (-1);
 	if (**fptr == 'h')
 		return (hotel(fptr, info));
 	if (**fptr == 'l')
