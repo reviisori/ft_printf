af276845d7b2a749a7598472ae00671ef14bd68d
home <aleksandr.tikka@gmail.com>
2022-05-13 10:28:10 +0300

    reminder for flag implementation

--- src/tc_ptr.c
+++ src/tc_ptr.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:12:42 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/09 13:59:36 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/13 08:54:06 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int	tc_ptr(t_vec *dest, t_stat *info)
 {
+	//#
 	info->lenght = l;
 	if (ft_vecncat(dest, "0x", 2) < 0)
 		return (-1);
