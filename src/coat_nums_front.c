5d76f570a83520a433017fe6424294d06370a6b0
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-16 13:51:34 +0300

    all negative values are now handled here
    resulted copypasta was just too messy, clunky and dirty

--- src/coat_nums_front.c
+++ src/coat_nums_front.c
@@ -6,24 +6,27 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 10:39:26 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/14 13:45:21 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/16 13:44:58 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-char	*format_sign(t_stat *info)
+static char	*format_sign(t_stat *info)
 {
 	char	*sign;
 
 	sign = NULL;
-	if (info->sign < 0)
-		sign = "-";
+	if (info->sign == -1)
+	{
+		sign = ft_strnew(1);
+		*sign = '-';
+	}
 	return (sign);
 }
 
-char	*format_preci(t_stat *info, size_t len)
+static char	*format_preci(t_stat *info, size_t len)
 {
 	char	*sign;
 	char	*pad;
@@ -32,7 +35,7 @@ char	*format_preci(t_stat *info, size_t len)
 	sign = format_sign(info);
 	if (info->preci > len)
 	{
-		len = info->preci - len; 
+		len = info->preci - len;
 		pad = ft_strnew(len);
 		ft_memset(pad, '0', len);
 		res = ft_strjoin(sign, pad);
@@ -42,17 +45,19 @@ char	*format_preci(t_stat *info, size_t len)
 	return (sign);
 }
 
-char	*coat_nums_front(t_stat *info, char **str, size_t len)
+/*
+static char	*format_prefix(t_stat *info, size_t len)
+{
+	return ();
+}
+*/
+
+char	*coat_nums_front(t_stat *info, size_t len)
 {
 	char	*pad;
 
 	pad = NULL;
 	//format_prefix();
-	if (**str == '-')
-	{
-		(*str)++;
-		len -= 1;
-	}
 	pad = format_preci(info, len);
 	//format_width();
 	return (pad);
