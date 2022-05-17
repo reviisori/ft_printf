7e9db9bb1221554ab98303df15765d5771672bb0
home <aleksandr.tikka@gmail.com>
2022-05-17 22:02:42 +0300

    ptr prefix added elsewhere now to work with width

--- src/tc_ptr.c
+++ src/tc_ptr.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:12:42 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/13 08:54:06 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/17 21:41:02 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,5 @@ int	tc_ptr(t_vec *dest, t_stat *info)
 {
 	//#
 	info->lenght = l;
-	if (ft_vecncat(dest, "0x", 2) < 0)
-		return (-1);
 	return (tc_num(dest, info));
 }
