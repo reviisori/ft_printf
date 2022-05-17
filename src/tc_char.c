81a3b8e143ae07af0ff69dcb1c070a6c70e393bb
Aleksandr Tikka <altikka@student.hive.fi>
2022-05-17 19:42:05 +0300

    has both pad locations in place now

--- src/tc_char.c
+++ src/tc_char.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:06:47 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/09 13:58:51 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/17 19:39:21 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ int	tc_char(t_vec *dest, t_stat *info)
 	char	c;
 
 	c = va_arg(info->ap, int);
+	if (pad(dest, info->width - 1, info->pad, !(info->dash)) < 0)
+		return (-1);
 	if (ft_vecpush(dest, &c) < 0)
+		return (-1);
+	if (pad(dest, info->width - 1, info->pad, (info->dash)) < 0)
 		return (-1);
 	return (1);
 }
