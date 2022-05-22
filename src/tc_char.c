56cb0832d53ce4d2f108ce85d927ddd194f0ebbd
home <aleksandr.tikka@gmail.com>
2022-05-22 09:47:51 +0300

    all chars now go through pad_char also to make '%%' case work seamlessly

--- src/tc_char.c
+++ src/tc_char.c
@@ -6,18 +6,15 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:06:47 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/18 15:24:44 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/22 09:29:07 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "vec.h"
 
-int	tc_char(t_vec *dest, t_stat *info)
+int	pad_char(t_vec *dest, t_stat *info, char c)
 {
-	char	c;
-
-	c = va_arg(info->ap, int);
 	if (pad(dest, (info->width - 1), info->pad, !(info->dash)) < 0)
 		return (-1);
 	if (ft_vecpush(dest, &c) < 0)
@@ -25,4 +22,12 @@ int	tc_char(t_vec *dest, t_stat *info)
 	if (pad(dest, (info->width - 1), info->pad, (info->dash)) < 0)
 		return (-1);
 	return (1);
+}
+
+int	tc_char(t_vec *dest, t_stat *info)
+{
+	char	c;
+
+	c = va_arg(info->ap, int);
+	return (pad_char(dest, info, c));
 }
