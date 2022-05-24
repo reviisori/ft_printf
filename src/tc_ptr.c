85e87ad02ed9a08d46b6d9b0a6c8bfd5e4b25f7c
home <aleksandr.tikka@gmail.com>
2022-05-24 09:47:08 +0300

    pointer's prefix now treated as if hash flag was ON

--- src/tc_ptr.c
+++ src/tc_ptr.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 16:12:42 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/17 21:41:02 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/24 09:26:08 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	tc_ptr(t_vec *dest, t_stat *info)
 {
-	//#
+	info->hash = true;
 	info->lenght = l;
 	return (tc_num(dest, info));
 }
