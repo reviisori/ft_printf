fce6468908998219ab768c0f5745308ab983a0b5
home <aleksandr.tikka@gmail.com>
2022-05-26 12:18:43 +0300

    overhauled to work with asterisk
    divided into 2 functions for asterisk support. had to invert compared to inves_preci to work when asterisk AND width given at the same time...

--- src/inves_width.c
+++ src/inves_width.c
@@ -6,26 +6,52 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 13:53:49 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/16 21:48:58 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/26 11:55:04 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-int	inves_width(const char **fptr, t_stat *info)
+static int	inves_width_asterisk(const char **fptr, t_stat *info)
 {
+	int		width;
 	size_t	hop;
 
-	if (!(ft_isdigit(**fptr)))
-		return (-1);
-	info->width = (unsigned int ) ft_atoi(*fptr);
-	if (info->width)
+	if (**fptr == '*')
 	{
-		hop = ft_anylen(info->width, 10, 1);
+		(*fptr)++;
+		width = va_arg(info->ap, int);
+		if (!(ft_isdigit(**fptr)))
+			return (width);
+	}
+	width = ft_atoi(*fptr);
+	if (width)
+	{
+		hop = ft_anylen(width, 10, 1);
 		(*fptr) = (*fptr) + hop;
 	}
 	while (**fptr == '0')
 		(*fptr)++;
+	return (width);
+}
+
+int	inves_width(const char **fptr, t_stat *info)
+{
+	int	width;
+
+	width = inves_width_asterisk(fptr, info);
+	if (width < 0)
+	{
+		info->dash = true;
+		//quick & dirty strikes again
+		if (info->zero && info->dash)
+		{
+			info->zero = false;
+			info->pad = ' ';
+		}
+		width *= -1;
+	}
+	info->width = (unsigned int ) width;
 	return (1);
 }
