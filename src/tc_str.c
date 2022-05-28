034a19f7e25f341511b76b8540ba6dbf0a4bd482
home <aleksandr.tikka@gmail.com>
2022-05-28 11:01:59 +0300

    separated padding of the str into it's own function
    can now be accessed from other functions for example to handle NaN in tc_dbl

--- src/tc_str.c
+++ src/tc_str.c
@@ -6,13 +6,24 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 15:45:28 by altikka           #+#    #+#             */
-/*   Updated: 2022/05/18 15:27:54 by altikka          ###   ########.fr       */
+/*   Updated: 2022/05/28 10:21:48 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 #include "vec.h"
+
+int	pad_str(t_vec *dest, t_stat *info, char *str, size_t len)
+{
+	if (pad(dest, (info->width - len), info->pad, !(info->dash)) < 0)
+		return (-1);
+	if (ft_vecncat(dest, str, len) < 0)
+		return (-1);
+	if (pad(dest, (info->width - len), info->pad, (info->dash)) < 0)
+		return (-1);
+	return (1);
+}
 
 int	tc_str(t_vec *dest, t_stat *info)
 {
@@ -25,11 +36,5 @@ int	tc_str(t_vec *dest, t_stat *info)
 	len = ft_strlen(str);
 	if (info->preci_on && info->preci < len)
 		len = info->preci;
-	if (pad(dest, (info->width - len), info->pad, !(info->dash)) < 0)
-		return (-1);
-	if (ft_vecncat(dest, str, len) < 0)
-		return (-1);
-	if (pad(dest, (info->width - len), info->pad, (info->dash)) < 0)
-		return (-1);
-	return (1);
+	return (pad_str(dest, info, str, len));
 }
